0a6eda4a4e46df6470c7dc02a52e2cbc860fe5fc
Evelyn Li <evelynli61205@gmail.com>
2021-08-26 15:53:16 -0700

    Make grid size dynamic (instead of hard-coded)

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const grid = document.querySelector('.grid')
 const clearBtn = document.querySelector('#clear')
+const slider = document.querySelector('input[type="range"]')
 
 let gridSize = 16
 createGrid(gridSize)
+displayGridSize()
 
-clearBtn.addEventListener('click', function () {
-    const squares = document.querySelectorAll('.square')
-    for (let square of squares) {
-        square.style.backgroundColor = ''
-    }
-})
+clearBtn.addEventListener('click', clearGrid)
+slider.addEventListener('input', displayGridSize)
+slider.addEventListener('change', changeGridSize)
 
 function createGrid(size) {
     document.documentElement.style.setProperty('--grid-size', size)
@@ -19,6 +18,26 @@ function createGrid(size) {
         square.classList.add('square')
         grid.appendChild(square)
         square.addEventListener('mouseenter', changeColor)
+    }
+}
+
+function changeGridSize() {
+    gridSize = slider.value
+    const squares = document.querySelectorAll('.square')
+    for (let square of squares) {
+        square.remove()
+    }
+    createGrid(gridSize)
+}
+
+function displayGridSize() {
+    document.querySelector('label').textContent = `Grid Size: ${slider.value} x ${slider.value}`
+}
+
+function clearGrid() {
+    const squares = document.querySelectorAll('.square')
+    for (let square of squares) {
+        square.style.backgroundColor = ''
     }
 }
 
